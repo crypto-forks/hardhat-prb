d3e51081e9cb1d0bf1645d8125ce55ce3001fae8
Paul Razvan Berg <hello@paulrberg.com>
2021-09-28 11:16:36 -0400

    fix(math): intrusive type extensions
    build(math): add "istanbul-reports" as dev dep
    refactor(math): define "toEbn" and "toMbn" at the top level
    refactor(math): import ethers "BigNumber" type as "EthersBigNumber"
    refactor(math): "toFp" to "toEbn"
    refactor(math): use "fromFp" from "evm-bn" instead of dividing by "SCALE"

--- packages/math/src/type-extensions.ts
+++ packages/math/src/type-extensions.ts
@@ -1,22 +1,6 @@
 import "hardhat/types/runtime";
 
-import type { BigNumber } from "@ethersproject/bignumber";
-import type { BigNumber as MathjsBigNumber } from "mathjs";
-
 import type { HardhatPRBMath } from "./HardhatPRBMath";
-
-declare module "@ethersproject/bignumber" {
-  interface BigNumber {
-    toMbn(): MathjsBigNumber;
-  }
-}
-
-declare module "decimal.js" {
-  interface Decimal {
-    toFp(): BigNumber;
-  }
-}
-
 declare module "hardhat/types/runtime" {
   export interface HardhatRuntimeEnvironment {
     prb: {
